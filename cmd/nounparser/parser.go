d0eb8efa6c08d954f8e82e5e7ad4b0328d6c9346
marthjod@gmail.com <marthjod@gmail.com>
2018-06-09 19:07:30 +0200

    nounparser: better logging

--- cmd/nounparser/parser.go
+++ cmd/nounparser/parser.go
@@ -3,9 +3,9 @@ package main
 import (
 	"bytes"
 	"context"
-	"log"
 	"net"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/marthjod/binquiry-experimental/noun"
 	pb "github.com/marthjod/binquiry-experimental/noun"
 	"github.com/marthjod/binquiry-experimental/pkg/reader"
@@ -21,24 +21,33 @@ const (
 type server struct{}
 
 func (s *server) Parse(ctx context.Context, in *pb.ParseRequest) (*pb.ParseResponse, error) {
+	log.Info("received parse request")
 	header, _, xmlRoot, err := reader.Read(bytes.NewReader(in.Word))
 	if err != nil {
+		log.Error(err)
 		return nil, err
 	}
 	path := xmlpath.MustCompile("//tr/td[2]")
 	word := noun.ParseNoun(header, path.Iter(xmlRoot))
+	log.Infof("returning %q", word.CanonicalForm())
 	return &pb.ParseResponse{Noun: word}, nil
 }
 
 func main() {
+	formatter := &log.TextFormatter{
+		FullTimestamp: true,
+	}
+	log.SetFormatter(formatter)
+
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatal("failed to listen: ", err)
 	}
 	s := grpc.NewServer()
 	pb.RegisterNounParserServer(s, &server{})
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
+	log.Infof("listening on %s", port)
 	if err := s.Serve(lis); err != nil {
 		log.Fatal("failed to serve: ", err)
 	}
